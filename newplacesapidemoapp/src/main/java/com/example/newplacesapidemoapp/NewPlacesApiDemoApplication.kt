add65ce964e9009f80abaf89e179ade5612d92eb
Dale Hawkins <dkhawk@google.com>
2024-02-09 16:03:00 -0700

    New api demo app structure in place

---
+++ newplacesapidemoapp/src/main/java/com/example/newplacesapidemoapp/NewPlacesApiDemoApplication.kt
@@ -0,0 +1,7 @@
+package com.example.newplacesapidemoapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NewPlacesApiDemoApplication : Application()
