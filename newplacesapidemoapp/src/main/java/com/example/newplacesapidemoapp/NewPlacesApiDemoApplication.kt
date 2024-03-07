c04a4a19620716c922a403748cf2ba00a85ce46f
dkhawk <dkhawk@google.com>
2024-03-07 13:23:43 -0700

    Removes initial new places api demoapp

--- newplacesapidemoapp/src/main/java/com/example/newplacesapidemoapp/NewPlacesApiDemoApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.example.newplacesapidemoapp
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class NewPlacesApiDemoApplication : Application()
