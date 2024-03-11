5aa4b4d489ba84cd7ffc8b05f6f6187fbc3c11dc
Dale Hawkins <dkhawk@google.com>
2024-03-11 15:56:51 -0600

    Move PlacesAutocomplete to place-ktx library

--- new-places-client/src/main/java/com/example/new_places_client/widget/ExperimentalPlacesApi.kt
+++
@@ -1,8 +0,0 @@
-package com.example.new_places_client.widget
-
-@RequiresOptIn(
-  "This material API is experimental and is likely to change or to be removed in" +
-    " the future."
-)
-@Retention(AnnotationRetention.BINARY)
-annotation class ExperimentalPlacesApi
